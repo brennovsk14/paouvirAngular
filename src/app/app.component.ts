23abdb93d13ba60bbace4a5eb6145ac3f07bb69a
Brenno Ramos <brennoramos231@gmail.com>
2026-02-24 16:03:21 -0300

    correções de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LoginComponent} from "./component/login/login.component";
+import {CadastroComponent} from "./component/cadastro/cadastro.component";
+import {MenuComponent} from "./component/menu/menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, CadastroComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./component/login/login.component";
+import {CadastroComponent} from "./component/cadastro/cadastro.component";
+import {MenuComponent} from "./component/menu/menu.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+  {
+    path: '',
+    component: LoginComponent, // Layout público para rotas não autenticadas
+
+  },
+  {
+    path: 'cadastro',
+    component: CadastroComponent, // Layout público para rotas não autenticadas
+
+  },
+  {
+    path: 'menu',
+    component: MenuComponent, // Layout público para rotas não autenticadas
+
+  },
+
+];
