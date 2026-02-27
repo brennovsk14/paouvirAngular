bc644840b0b100440e6352cb1602db27fc125e2f
Brenno Ramos <brennoramos231@gmail.com>
2026-02-27 15:51:27 -0300

    Novos componentes

---
+++ src/app/views/view-cadastro/view-cadastro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-cadastro',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-cadastro.component.html',
+  styleUrl: './view-cadastro.component.css'
+})
+export class ViewCadastroComponent {
+
+}
